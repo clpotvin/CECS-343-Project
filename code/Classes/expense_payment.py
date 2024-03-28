bc3f25b50ac16177c983d1c07bc91b3b0dd26ffb
Tanner Chesley <159854573+NugBugget@users.noreply.github.com>
2024-03-27 20:37:42 -0700

    Update expense_payment.py

--- code/Classes/expense_payment.py
+++ code/Classes/expense_payment.py
@@ -1,1 +1,26 @@
 
+
+class ExpensePayment:
+
+  def __init__(expense_amount,date,reason):
+    self.expense_amount = expense_amount
+    self.date = date
+    self.reason = reason
+
+  def get_expense_amount():
+    return self.expense_amount
+
+  def get_expense_date():
+    return self.date
+
+  def get_reason():
+    return self.reason
+
+  def set_reason(reason):
+    self.reason = reason
+
+  def set_expense_amount(expense_amount):
+    self.expense_amount = expense_amount
+
+
+  
