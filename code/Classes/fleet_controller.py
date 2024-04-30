9bde5b887acbe175707539b0a777a4db9fb5b833
Tanner <kekaulike88@gmail.com>
2024-04-30 16:17:15 -0700

    added temporary UI setup

--- code/Classes/fleet_controller.py
+++
@@ -1,3 +0,0 @@
-# This class should manage the vehicles in the fleet, tracking what is available, what is booked, etc.
-# - Data should be processed by this class, then stored in an xml file
-# - Data might include reservations, current vehicle availability, vehicle damage, etc.
